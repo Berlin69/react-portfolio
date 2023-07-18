b7cb3638d72ce6cd6b251503b5c3f032a27d0ece
Maksim Opriatin <maksim.opryatin@yandex.ru>
2023-07-18 19:18:00 +0300

    feat: info component

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
+import Info from './components/Info/Info';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Info />
+    </>
   );
 }
 
